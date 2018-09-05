6a936ff3a4f03c23fafedf90e4ab3949a28ef44e
birnstiel@me.com <birnstiel@me.com>
2018-09-05 20:19:20 +0200

    improved the widget, now allows clicking on lines; improved layout

--- results_widget.py
+++ results_widget.py
@@ -35,9 +35,9 @@ with open('data.csv') as fid:
 
 # Make the figure
 
-f = plt.figure()
+fig = plt.figure()
 
-ax = f.add_axes([0.1, 0.5, 0.5, 0.4])
+ax = fig.add_axes([0.1, 0.3, 0.5, 0.7])
 
 # make up the isella profile
 
@@ -57,28 +57,41 @@ vf_values = sorted(list(set([d[2] for d in data])))
 # to avoid garbage collection
 ax._widgets = []
 
-# create buttons
+# CREATE BUTTONS
 
-sim_buttons = []
 button_width = 0.1
+button_lheigh = 0.07
+button_x0 = 0.05
+button_y0 = 0.05
 
-# create the axes for the button
-_ax = f.add_axes([0.1, 0.1, button_width, len(simnr_values) * button_width])
-sim_buttons = w.CheckButtons(_ax, [str(s) for s in simnr_values], [False for s in simnr_values])
-for r in sim_buttons.rectangles:
-    r.set_facecolor("blue")
-    r.set_edgecolor("k")
-    r.set_alpha(0.2)
+# 1. ALPHA
+n_buttons = len(alpha_values)
+ax_alpha = fig.add_axes([button_x0, button_y0, button_width, n_buttons * button_lheigh])
+alpha_buttons = w.CheckButtons(ax_alpha, [str(s) for s in alpha_values], [True] + (n_buttons - 1) * [False])
+ax_alpha.set_title(r'$\alpha$')
+ax._widgets += [alpha_buttons]  # avoids garbage collection
 
-ax._widgets += [sim_buttons]  # avoids garbage collection
+# 2. vf
+n_buttons = len(vf_values)
+ax_vf = fig.add_axes([button_x0 + button_width, button_y0, button_width, n_buttons * button_lheigh])
+vf_buttons = w.CheckButtons(ax_vf, [str(s) for s in vf_values], [True] + (n_buttons - 1) * [False])
+ax_vf.set_title(r'$v_\mathrm{frag}$')
+ax._widgets += [vf_buttons]  # avoids garbage collection
+
+info_dict = {}
 
 
 def callback(event):
     """
-    The callback for the simulation buttons
+    The callback for updating the figure when the buttons are clicked
     """
-    states = sim_buttons.get_status()
-    selected_values = np.array(simnr_values)[states]
+    # get masks for all the selected values
+
+    alpha_states = alpha_buttons.get_status()
+    alpha_selected = np.array(alpha_values)[alpha_states]
+
+    vf_states = vf_buttons.get_status()
+    vf_selected = np.array(vf_values)[vf_states]
 
     # delete all simulation lines (keeping the observational line)
 
@@ -86,17 +99,33 @@ def callback(event):
         _line.remove()
 
     # plot all lines that match the selected values
-
+    # in info_dict, we store a information string with the parameter
+    # values for every line
+    info_dict.clear()
     for d in data:
-        if d[0] in selected_values:
-            ax.plot(d[-2], d[-1])
+        if d[1] in alpha_selected and d[2] in vf_selected:
+            l, = ax.plot(d[-2], d[-1], 'r', picker=True)
+            info_dict[l] = 'alpha = {}, vf = {}'.format(d[1], d[2])
 
     plt.draw()
 
 
+def onpick(event):
+    """
+    Function that gets called if a line is clicked
+    """
+    if isinstance(event.artist, plt.Line2D):
+        print('parameters of this line: ', info_dict[event.artist])
+
+
+fig.canvas.mpl_connect('pick_event', onpick)
+
 # now link the callback to the buttons
 
-sim_buttons.on_clicked(callback)
+alpha_buttons.on_clicked(callback)
+vf_buttons.on_clicked(callback)
 
+# call the callback function once to make the plot agree with state of the buttons
+callback(None)
 
 plt.show()
