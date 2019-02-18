00ef0b8ab9ce32a0bbcc6c03526c42d583a58b93
birnstiel@me.com <birnstiel@me.com>
2019-02-18 17:27:32 +0100

    created a surface density widget

--- results_widget.py
+++ results_widget.py
@@ -1,134 +1,186 @@
 import matplotlib.pyplot as plt
-import matplotlib.widgets as w
 import numpy as np
+import glob
+from matplotlib.widgets import Slider
+import astropy.constants as u
+from scipy.interpolate import interp2d
 
-# Get the data
+au = u.au.cgs.value
 
-data = []
+# read in hydro data
 
-with open('data.csv') as fid:
-    while True:
-        line = fid.readline()
-        if line.strip().startswith('#'):
-            continue
+hydro_r = np.loadtxt('data_evolving/domain_y.dat')
+files = sorted(glob.glob('data_evolving/*.txt'))
+hydro_data = np.array([np.loadtxt(file) for file in files])
+times = np.array([float(file[-11:-4]) for file in files])
+times /= times[-1]
 
-        if line.strip() == '':
-            break
+# define hydro interpolation function
 
-        line = line.strip().split(',')
-        sim_nr = int(line.pop(0))
-        alpha = float(line.pop(0))
-        vf = float(line.pop(0))
-        nr = int(line.pop(0))
-        r = [float(line.pop(0)) for i in range(nr)]
-        Inu = [float(line.pop(0)) for i in range(nr)]
+f2d = interp2d(np.log10(hydro_r), times, np.log10(hydro_data))
 
-        data += [[
-            sim_nr,
-            alpha,
-            vf,
-            nr,
-            r,
-            Inu
-            ]]
 
+def get_hydro(r, t):
+    """
+    Get hydro profile by interpolating the data.
+
+    r : array
+        radial array to interpolate onto
+
+    t : float
+        time at which the profile is interpolated
+
+    Output
+    ------
+
+    array: gas surface density on hydro grid
+    """
+    return 10**f2d(np.log10(r), t)
+
+
+def get_fit(r, sig0, t, masses, radii):
+    """
+    HERE YOU HAVE TO WRITE YOUR OWN KANAGAWA FUNCTION
+    """
+    # start with the initial profile
+    sig = sig0.copy()
+
+    # for each planet: multiply a factor to represent the gap shape
+
+    for mp, rp in zip(masses, radii):
+        sig *= 1 - t * mp * np.exp(-(r - rp)**2 / (2 * (5 * au)**2))
+
+    return sig
+
+
+# arbitrary initial condition
+
+r = np.logspace(1, np.log10(300), 300) * au
+sig_0 = get_hydro(r, 0)
 
 # Make the figure
 
 fig = plt.figure()
-
-ax = fig.add_axes([0.1, 0.3, 0.5, 0.6])
-
-# make up the isella profile
-
-r_isella = [0.5, 3, 10, 15]
-Inu_isella = [20, 10, 12, 8]
-
-# plot the isella profile
-
-ax.plot(r_isella, Inu_isella, 'k--')
-
-# now we need to make a variable number of buttons
-
-simnr_values = sorted(list(set([d[0] for d in data])))
-alpha_values = sorted(list(set([d[1] for d in data])))
-vf_values = sorted(list(set([d[2] for d in data])))
+ax = fig.add_axes([0.1, 0.3, 0.8, 0.65])
+l1, = ax.loglog(hydro_r / au, hydro_data[0], 'k-', label='hydro')
+l2, = ax.loglog(r / au, sig_0, 'r', label='fit')
+ax.legend(fontsize='small')
+ax.set_xlim(r[[0, -1]] / au)
+ax.set_ylim(1e-3, 1e3)
 
 # to avoid garbage collection
+
 ax._widgets = []
 
-# CREATE BUTTONS
+x0 = ax.get_position().x0
+x1 = ax.get_position().x1
+y0 = ax.get_position().y0
+y1 = ax.get_position().y1
+width = x1 - x0
+height = y1 - y0
 
-button_width = 0.1
-button_lheigh = 0.07
-button_x0 = 0.05
-button_y0 = 0.05
+# CREATE SLIDERS
 
-# 1. ALPHA
-n_buttons = len(alpha_values)
-ax_alpha = fig.add_axes([button_x0, button_y0, button_width, n_buttons * button_lheigh])
-alpha_buttons = w.CheckButtons(ax_alpha, [str(s) for s in alpha_values], [True] + (n_buttons - 1) * [False])
-ax_alpha.set_title(r'$\alpha$')
-ax._widgets += [alpha_buttons]  # avoids garbage collection
+# time slider
+axTime = plt.axes([x0 + 0.15 * width,
+                   0.20,
+                   0.75 * width,
+                   0.05 * height], facecolor="lightgoldenrodyellow")
 
-# 2. vf
-n_buttons = len(vf_values)
-ax_vf = fig.add_axes([button_x0 + button_width, button_y0, button_width, n_buttons * button_lheigh])
-vf_buttons = w.CheckButtons(ax_vf, [str(s) for s in vf_values], [True] + (n_buttons - 1) * [False])
-ax_vf.set_title(r'$v_\mathrm{frag}$')
-ax._widgets += [vf_buttons]  # avoids garbage collection
+sliderTime = Slider(axTime, "Time", -1, 0, valinit=0, valfmt="%.2f")
+title = axTime.set_title('time = ')
+ax._widgets += [sliderTime]
 
-info_dict = {}
+# mass slider
+
+axMass1 = plt.axes([x0 + 0.15 * width,
+                    0.15,
+                    0.25 * width,
+                    0.05 * height], facecolor="lightgoldenrodyellow")
+
+axMass2 = plt.axes([x0 + 0.15 * width,
+                    0.10,
+                    0.25 * width,
+                    0.05 * height], facecolor="lightgoldenrodyellow")
+
+axMass3 = plt.axes([x0 + 0.15 * width,
+                    0.05,
+                    0.25 * width,
+                    0.05 * height], facecolor="lightgoldenrodyellow")
+
+sliderMass1 = Slider(axMass1, "$M_1$", 0, 1, valinit=0, valfmt="%.2f")
+sliderMass2 = Slider(axMass2, "$M_2$", 0, 1, valinit=0, valfmt="%.2f")
+sliderMass3 = Slider(axMass3, "$M_3$", 0, 1, valinit=0, valfmt="%.2f")
+
+ax._widgets += [sliderMass1]
+ax._widgets += [sliderMass2]
+ax._widgets += [sliderMass3]
+
+# position slider
+
+axRadius1 = plt.axes([x0 + 0.55 * width,
+                      0.15,
+                      0.25 * width,
+                      0.05 * height], facecolor="lightgoldenrodyellow")
+
+axRadius2 = plt.axes([x0 + 0.55 * width,
+                      0.10,
+                      0.25 * width,
+                      0.05 * height], facecolor="lightgoldenrodyellow")
+
+axRadius3 = plt.axes([x0 + 0.55 * width,
+                      0.05,
+                      0.25 * width,
+                      0.05 * height], facecolor="lightgoldenrodyellow")
+
+sliderRadius1 = Slider(axRadius1, "$R_1$", 1, 300, valinit=50, valfmt="%.2f")
+sliderRadius2 = Slider(axRadius2, "$R_2$", 1, 300, valinit=100, valfmt="%.2f")
+sliderRadius3 = Slider(axRadius3, "$R_3$", 1, 300, valinit=150, valfmt="%.2f")
+
+ax._widgets += [sliderRadius1]
+ax._widgets += [sliderRadius2]
+ax._widgets += [sliderRadius3]
 
 
 def callback(event):
     """
     The callback for updating the figure when the buttons are clicked
     """
-    # get masks for all the selected values
 
-    alpha_states = alpha_buttons.get_status()
-    alpha_selected = np.array(alpha_values)[alpha_states]
+    # get all values from the sliders
 
-    vf_states = vf_buttons.get_status()
-    vf_selected = np.array(vf_values)[vf_states]
+    time = 10**sliderTime.val
+    M1 = sliderMass1.val
+    M2 = sliderMass2.val
+    M3 = sliderMass3.val
+    R1 = sliderRadius1.val * au
+    R2 = sliderRadius2.val * au
+    R3 = sliderRadius3.val * au
 
-    # delete all simulation lines (keeping the observational line)
+    # update log sliders with a meaningful value
 
-    for _line in ax.get_lines()[1:]:
-        _line.remove()
+    title.set_text(f'time = {time:.2g}')
 
-    # plot all lines that match the selected values
-    # in info_dict, we store a information string with the parameter
-    # values for every line
-    info_dict.clear()
-    for d in data:
-        if d[1] in alpha_selected and d[2] in vf_selected:
-            l, = ax.plot(d[-2], d[-1], '0.5', picker=True)
-            info_dict[l] = r'$\alpha$' + ' = {}, $v_\mathrm{{frag}}$ = {}'.format(d[1], d[2])
+    # get the new profiles
 
-    ax.set_title('click on a line to see its parameters!', fontsize='small')
-    plt.draw()
+    sig_hydro = get_hydro(hydro_r, time)
+    sig_fit = get_fit(r, sig_0, time, [M1, M2, M3], [R1, R2, R3])
 
+    # update the lines with the new profiles
 
-def onpick(event):
-    """
-    Function that gets called if a line is clicked
-    """
-    if isinstance(event.artist, plt.Line2D):
-        ax.set_title('parameters of this line: ' + info_dict[event.artist], fontsize='small')
-        for line in ax.get_lines()[1:]:
-            line.set_color('0.5')
-        event.artist.set_color('r')
-        plt.draw()
+    l1.set_ydata(sig_hydro)
+    l2.set_ydata(sig_fit)
 
 
-fig.canvas.mpl_connect('pick_event', onpick)
+# connect all sliders to the callback function
 
-# now link the callback to the buttons
-
-alpha_buttons.on_clicked(callback)
-vf_buttons.on_clicked(callback)
+sliderTime.on_changed(callback)
+sliderMass1.on_changed(callback)
+sliderMass2.on_changed(callback)
+sliderMass3.on_changed(callback)
+sliderRadius1.on_changed(callback)
+sliderRadius2.on_changed(callback)
+sliderRadius3.on_changed(callback)
 
 # call the callback function once to make the plot agree with state of the buttons
 callback(None)
